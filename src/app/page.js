1092fbf858d8a6567a893f212e9318a4051eeafd
Your Name <gdikshan10@gmail.com>
2024-04-30 01:24:37 +0545

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h2>this is the index page of the imdb clone wher you can watch movie</h2>
+      </div>
+    </main>
+  );
+}
