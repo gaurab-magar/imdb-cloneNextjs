1f3a2beb05a7630b3566023457563a9c55fd27dc
Your Name <gdikshan10@gmail.com>
2024-05-16 01:11:23 +0545

    suspence added to the navbaritem page

--- src/app/Components/NavbarItem.js
+++ src/app/Components/NavbarItem.js
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react'
+import React, { Suspense } from 'react'
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation';
 
@@ -7,9 +7,10 @@ const NavbarItem = ({title,param}) => {
   const searchParams = useSearchParams();
   const genre = searchParams.get('genre');
   return (
-    <div>
-       <Link href={`/?genre=${param}`} className={`transition-all duration-200 hover:text-blue-400 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-2 decoration-blue-500 rounded-lg text-blue-500 transition-all duration-200':''}`} >{title}</Link>
-    </div>
+    <Suspense>
+
+      <Link href={`/?genre=${param}`} className={`transition-all duration-200 hover:text-blue-400 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-2 decoration-blue-500 rounded-lg text-blue-500 transition-all duration-200':''}`} >{title}</Link>
+    </Suspense>
   )
 }
 
